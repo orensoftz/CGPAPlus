fe841cdd27576ee3d2828af43a51adc25701556b
orensoftz <orensoftz@gmail.com>
2017-03-10 12:11:19 +0800

    Second Commit

--- app/src/main/java/com/orensoftz/cgpaplus/MainActivity.java
+++ app/src/main/java/com/orensoftz/cgpaplus/MainActivity.java
@@ -1,7 +1,7 @@
 package com.orensoftz.cgpaplus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("hello world");
+        System.out.println("hello world 2");
     }
 }
